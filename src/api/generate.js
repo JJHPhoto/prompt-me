ea61471a217f54e2d589d7a6e76f4fa8517fe369
Joshua J. Haller <phone@joshuajhaller.com>
2022-05-22 14:53:55 +1000

    Stopping.

--- src/api/generate.js
+++ src/api/generate.js
@@ -1,4 +1,4 @@
-const { Configuration, OpenAIApi } = require("openai");
+import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
@@ -7,26 +7,23 @@ const openai = new OpenAIApi(configuration);
 
 export default async function (req, res) {
   const completion = await openai.createCompletion("text-curie-001", {
-    prompt: generatePrompt(req.body.user),
-    temperature: 0.7,
+    prompt: generatePrompt(req.body.country),
+    temperature: 0.6,
     max_tokens: 200,
-    top_p: 1,
-    frequency_penalty: 0,
-    presence_penalty: 0,
   });
   res.status(200).json({ result: completion.data.choices[0].text });
 }
 
-function generatePrompt(user) {
-  return `"What was the first empire in history?\n\nThe first empire in history was the Assyrian Empire.\n\nWho succeeded them?\n\nThe succeeding empire was the Persian Empire.\n\nNot the Babylonians?\n\nThe Babylonians did not succeed the Assyrian Empire, but were a predecessor of that empire.\n\nDid Babylon have an empire?\n\nNo, Babylon did not have an empire.\n\nWhat about Egypt?\n\nEgypt was not an empire, but had a long history of being an influential culture and power in the region.\n\nWhy was Assyria an empire?\n\nThe Assyrian Empire was an empire because it was a large and powerful kingdom that controlled a large area of the world.",`;
-}
+function generatePrompt(country) {
+  const capitalizedCountry =
+    country[0].toUpperCase() + country.slice(1).toLowerCase();
 
-// const response = await openai.createCompletion("text-curie-001", {
-//   prompt:
-//     "What was the first empire in history?\n\nThe first empire in history was the Assyrian Empire.\n\nWho succeeded them?\n\nThe succeeding empire was the Persian Empire.\n\nNot the Babylonians?\n\nThe Babylonians did not succeed the Assyrian Empire, but were a predecessor of that empire.\n\nDid Babylon have an empire?\n\nNo, Babylon did not have an empire.\n\nWhat about Egypt?\n\nEgypt was not an empire, but had a long history of being an influential culture and power in the region.\n\nWhy was Assyria an empire?\n\nThe Assyrian Empire was an empire because it was a large and powerful kingdom that controlled a large area of the world.",
-//   temperature: 0.7,
-//   max_tokens: 200,
-//   top_p: 1,
-//   frequency_penalty: 0,
-//   presence_penalty: 0,
-// });
+  return `Write a description of a country based on these inputs.
+
+  Name: Australia
+  Description: Australia is a country located in the southern hemisphere. It is the sixth-largest country by total area and has the world's tenth-largest population. Australia is bordered by Indonesia and Papua New Guinea to the north, the Indian Ocean to the east, Tasmania and New Zealand to the south, and South Africa to the west.
+  Name: Scotland
+  Description: Scotland is a country located in western Europe. It has a population of just over 5 million and an area of just over 6,000 square miles. Scotland is bordered by England to the south and west, and by the North Sea to the north.
+  Name:${capitalizedCountry}
+  Description:`;
+}
--- src/components/Prompt.js
+++ src/components/Prompt.js
@@ -1,122 +1,41 @@
 import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 // import api from "../api/generate"
 
-// import React, { Component } from "react";
-// import { Form, Button, Card } from "react-bootstrap";
-// const { Configuration, OpenAIApi } = require("openai");
-// require("dotenv").config();
-
-export default function Prompt() {
-  const [userInput, setUserInput] = useState("");
+export default function Home() {
+  const [countryInput, setCountryInput] = useState("");
   const [result, setResult] = useState();
 
   async function onSubmit(event) {
     event.preventDefault();
-    const response = await fetch("./api/generate", {
+    const response = await fetch("../api/generate", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ user: userInput }),
+      body: JSON.stringify({ country: countryInput }),
     });
     const data = await response.json();
     setResult(data.result);
-    setUserInput("");
+    setCountryInput("");
   }
 
   return (
     <div>
-      <h1> Header here?</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          type="text"
-          name="user"
-          placeholder="Ask a question about history"
-          value={userInput}
-          onChange={(e) => setUserInput(e.target.value)}
-        />
-      </form>
-      console.log(userInput)
+      <main>
+        <h3>Gather information</h3>
+        <form onSubmit={onSubmit}>
+          <input
+            type="text"
+            name="command"
+            placeholder="Learn about locations"
+            value={countryInput}
+            onChange={(e) => setCountryInput(e.target.value)}
+          />
+          <input type="submit" value="Find Out" />
+        </form>
+        <div>{result}</div>
+      </main>
     </div>
   );
 }
-
-// class Prompt extends Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       heading: "AI response here...",
-//       response: "...coming in a moment",
-//     };
-//   }
-
-//   onFormSubmit = (e) => {
-//     e.preventDefault();
-
-//     const promptData = new PromptData(e.target),
-//       promptDataObj = Object.promptEntries(promptData.entries());
-//     console.log(promptDataObj.userPrompt);
-
-//     const configuration = new Configuration({
-//       apiKey: "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",
-//       // apiKey: process.env.OPENAI_API_KEY,
-//     });
-//     const openai = new OpenAIApi(configuration);
-
-//     openai
-//       .createCompletion("text-curie-001", {
-//         prompt:
-//           "What was the first empire in history?\n\nThe first empire in history was the Assyrian Empire.\n\nWho succeeded them?\n\nThe succeeding empire was the Persian Empire.\n\nNot the Babylonians?\n\nThe Babylonians did not succeed the Assyrian Empire, but were a predecessor of that empire.\n\nDid Babylon have an empire?\n\nNo, Babylon did not have an empire.\n\nWhat about Egypt?\n\nEgypt was not an empire, but had a long history of being an influential culture and power in the region.\n\nWhy was Assyria an empire?\n\nThe Assyrian Empire was an empire because it was a large and powerful kingdom that controlled a large area of the world.",
-//         temperature: 0.7,
-//         max_tokens: 200,
-//         top_p: 1,
-//         frequency_penalty: 0,
-//         presence_penalty: 0,
-//       })
-//       .then((response) => {
-//         this.setState({
-//           heading: `AI answers from your quetions are: ${promptDataObj.userPrompt}`,
-//           response: `${response.data.choices[0].text}`,
-//         });
-//       });
-
-//     // this.setState({
-//     //   heading: `Ask our AI about some history!`
-//     //   response: `The response from our AI will be appear after you submit your question.`
-//     // })
-//   };
-
-//   render() {
-//     return (
-//       <div>
-//         <Form onSubmit={this.onFormSubmit}>
-//           <Form.Group className="mb-3" controlId="formPrompt">
-//             <Form.Label>What would you like to learn?</Form.Label>
-//             <Form.Control
-//               type="text"
-//               name="userPrompt"
-//               placeholder="Enter your question here"
-//             />
-//             <Form.Text className="text-muted">
-//               See what you can learn from an AI
-//               {/* Change this to text input and make the box larger? */}
-//             </Form.Text>
-//           </Form.Group>
-//           <Button variant="primary" size="lg" type="submit">
-//             Learn!
-//           </Button>
-//         </Form>
-
-//         <Card>
-//           <Card.Header>Saved Prompts?</Card.Header>
-//           <Card.Body>
-//             <Card.Title>{this.state.heading}</Card.Title>
-//             <Card.Text>{this.state.response}</Card.Text>
-//           </Card.Body>
-//         </Card>
-//       </div>
-//     );
-//   }
-// }
-
-// export default Prompt;
